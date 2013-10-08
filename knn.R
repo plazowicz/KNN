1daeee16046e3b22b9c5176b03d20dc5c2288f14
Mateusz Opala <mateuszjacekopala@gmail.com>
2013-10-08 10:03:29 +0200

    Add breast cancer data, refactor code

--- knn.R
+++ knn.R
@@ -4,10 +4,13 @@ library(class)
 # Conf
 train.percentage <- 0.8
 
-# Load data
+# Load wine data
 wine.data <- read.table('Data/winequality-white.csv',sep=';',header=TRUE)
-
 wine.data <- data.matrix(wine.data)
+
+# Load breast cancer data
+cancer.data <- read.table('Data/breast-cancer-wisconsin.data',sep=',')
+cancer.data <- data.matrix(cancer.data[,2:dim(cancer.data)[2]])
 
 Normalize <- function(data) {
   means = apply(data, 2, mean)
@@ -21,38 +24,43 @@ Normalize <- function(data) {
   data
 }
 
-normalized.data <- Normalize(wine.data[,1:dim(wine.data)[2]-1])
-
-m <- dim(normalized.data)[1]
-
-# Prepare training data, test data and labels 
-
-train.count <- as.integer(m*train.percentage)
-
-train.data <- normalized.data[1:train.count,]
-test.data <- normalized.data[(train.count+1):m,]
-
-labels <- wine.data[,dim(wine.data)[2]]
-cl <- labels[1:train.count]
-correctTestLabels <- labels[(train.count+1):m]
-
-test.count <- length(correctTestLabels)
-
-errors <- 1:200
-
-for(n in 1:500) {
-  result <- knn(train.data, test.data, cl, k=n)
+DoKNNWithNumericalData <- function(data, plotFile, kSupremum=200) {
+  normalized.data <- Normalize(data[,1:dim(data)[2]-1])
+  m <- dim(data)[1]
   
-  errors.count <- 0
+  # Prepare training data, test data and labels 
   
-  for(i in 1:test.count) {
+  train.count <- as.integer(m*train.percentage)
+  
+  train.data <- normalized.data[1:train.count,]
+  test.data <- normalized.data[(train.count+1):m,]
+  
+  labels <- data[,dim(data)[2]]
+  cl <- labels[1:train.count]
+  correctTestLabels <- labels[(train.count+1):m]
+  
+  test.count <- length(correctTestLabels)
+  
+  errors <- 1:kSupremum
+  
+  for(n in 1:kSupremum) {
+    result <- knn(train.data, test.data, cl, k=n)
     
-    if( result[i] != correctTestLabels[i] ) {
-      errors.count <- errors.count + 1
+    errors.count <- 0
+    
+    for(i in 1:test.count) {
+      
+      if( result[i] != correctTestLabels[i] ) {
+        errors.count <- errors.count + 1
+      }
     }
+    
+    error.rate <- errors.count/test.count
+    errors[n] <- error.rate
   }
   
-  error.rate <- errors.count/test.count
-  errors[n] <- error.rate
+  plot(1:kSupremum, errors, main="Performance of KNN with regard to K on sample data set", xlab="K", ylab="Error rate")
+  savePlot(filename=paste("Plots",plotFile,sep="/"), type="jpeg")
 }
 
+DoKNNWithNumericalData(cancer.data, "cancer200.ext")
